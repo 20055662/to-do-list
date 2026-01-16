aa789b38112d3cebbc36a60e0a25d982903c10e5
jiaping-huang <kapinghuang@gmail.com>
2026-01-16 21:18:14 +0800

    feat: refactor with methods

--- ToDoList/ToDoList/Program.cs
+++ ToDoList/ToDoList/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 class Program
 {
@@ -9,44 +10,17 @@ class Program
 
         while (true) // menu loop
         {
-            Console.WriteLine("==== ToDo Menu ====");
-            Console.WriteLine("1. Add Task");
-            Console.WriteLine("2. View Tasks");
-            Console.WriteLine("3. Exit");
-            Console.Write("Choose an option: ");
-
+            ShowMenu();
             string choice = Console.ReadLine();
 
             switch (choice) //user choices
             {
                 case "1":
-                    Console.Write("Enter task: ");
-                    string task = Console.ReadLine();
-                    
-                    if (!string.IsNullOrWhiteSpace(task)) //input validation
-                    {
-                        tasks.Add(task);
-                        Console.WriteLine("Task added.");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Task cannot be empty.");
-                    }
+                    AddTask(tasks);
                     break;
 
                 case "2":
-                    Console.WriteLine("---- Tasks ----");
-                    if (tasks.Count == 0)
-                    {
-                        Console.WriteLine("No tasks");
-                    }
-                    else
-                    {
-                        for(int i=0; i< tasks.Count; i++) //for loop
-                        {
-                            Console.WriteLine($"{i + 1}.{tasks[i]}");// string interpolation
-                        }
-                    }
+                    ViewTasks(tasks);
                     break;
 
                 case "3":
@@ -61,5 +35,47 @@ class Program
 
         }
 
+    }
+
+    static void ShowMenu()
+    {
+        Console.WriteLine("==== ToDo Menu ====");
+        Console.WriteLine("1. Add Task");
+        Console.WriteLine("2. View Tasks");
+        Console.WriteLine("3. Exit");
+        Console.Write("Choose an option: ");
+
+    }
+
+    static void AddTask(List<string> tasks)
+    {
+        Console.Write("Enter task: ");
+        string task = Console.ReadLine();
+
+        if (!string.IsNullOrWhiteSpace(task)) //input validation
+        {
+            tasks.Add(task);
+            Console.WriteLine("Task added.");
+        }
+        else
+        {
+            Console.WriteLine("Task cannot be empty.");
+        }
+    }
+
+    static void ViewTasks(List<string> tasks)
+    {
+        Console.WriteLine("---- Tasks ----");
+        if (tasks.Count == 0)
+        {
+            Console.WriteLine("No tasks");
+        }
+        else
+        {
+            for (int i = 0; i < tasks.Count; i++) //for loop
+            {
+                Console.WriteLine($"{i + 1}.{tasks[i]}");// string interpolation
+            }
+        }
     }
 }
